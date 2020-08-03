3839060f64ab638989a3b933edc87ac26e97da53
Sudhakaran-S <sudhakaran.1801232@srec.ac.in>
2020-08-03 18:09:16 +0530

    first commit

--- schemas/users.js
+++ schemas/users.js
@@ -1,9 +1,10 @@
+
 const mongoose = require('mongoose')
 
 const { Schema } = mongoose;
 const UserSchema = new Schema({
-  name: String,
-  displayname: String,
+  //name: String,
+  //displayname: String,             //i have commented this since in login.js u are only sending email and password so only those should be there
   email: String,
   password: String
 })
